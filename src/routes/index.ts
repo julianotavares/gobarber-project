c3fd28f115cbafcc8f693f8b0fdd761cce5aca50
Juliano Tavares <juliano.tavaress@gmail.com>
2020-11-17 15:14:09 -0300

    remove old files2

--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import appointmentsRouter from './appointments.routes';
-
-const routes = Router();
-
-routes.use('/appointments', appointmentsRouter)
-
-export default routes;
