3ee1a6ccf6877489e26a179b24aac4461bf36b6c
Juliano Tavares <juliano.tavaress@gmail.com>
2020-11-22 20:27:00 -0300

    added signup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import GlobalStyles from './styles/global';
-import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
+// import SignIn from './pages/SignIn';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyles />
   </>
 );
--- frontend/src/pages/SignIn/styles.ts
+++ frontend/src/pages/SignIn/styles.ts
@@ -29,23 +29,6 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    button {
-      background: #ff9000;
-      height: 56px;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      color: #312e38;
-      width: 100%;
-      font-weight: 500;
-      margin-top: 16px;
-      transition: background-color 0.2s;
-
-      &:hover {
-        background: ${shade(0.2, '#FF9000')};
-      }
-    }
-
     a {
       color: #f4ede8;
       display: block;
