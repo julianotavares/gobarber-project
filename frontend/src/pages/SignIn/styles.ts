1a4c3f7dac3cacdd125d8eb5882744fb035c8d15
Juliano Tavares <juliano.tavaress@gmail.com>
2020-11-24 14:55:22 -0300

    Set up routes

--- frontend/src/pages/SignIn/styles.ts
+++ frontend/src/pages/SignIn/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signInBackgroundImg from '../../assets/sign-in-background.png';
@@ -15,10 +15,26 @@ export const Content = styled.div`
   flex-direction: column;
   align-items: center;
 
-  place-content: center;
+  justify-content: center;
 
   width: 100%;
   max-width: 700px;
+`;
+
+const apperFromLeft = keyframes`
+    from {
+      opacity: 0;
+      transform: translateX(-50px)
+    }
+  `;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${apperFromLeft} 1s;
 
   form {
     margin: 80px 0;
