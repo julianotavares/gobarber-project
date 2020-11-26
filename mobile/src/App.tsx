6590f214b593f2b19a8e44a1cfcd3732b99d60fe
Juliano Tavares <juliano.tavaress@gmail.com>
2020-11-26 18:51:17 -0300

    Setup statusbar style

---
+++ mobile/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { View, StatusBar } from 'react-native';
+
+const App: React.FC = () => (
+  <>
+  <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+
+    <View style={{flex: 1, backgroundColor: '#312e38'}}/>
+  </>
+)
+
+export default App;
