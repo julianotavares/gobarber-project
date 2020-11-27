67bac1c164c526afe041d02ef8bae6cae739094c
Juliano Tavares <juliano.tavaress@gmail.com>
2020-11-27 16:42:14 -0300

    Added Input and Button components

---
+++ mobile/src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { RectButtonProperties } from 'react-native-gesture-handler'
+
+import { Container, ButtonText } from './styles'
+
+interface ButtonProps {
+  children: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
+  <Container {...rest}>
+  <ButtonText>{children}</ButtonText>
+</Container>
+)
+
+
+
+export default Button
