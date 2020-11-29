f377068416c8387f71b0c3d92a8d87d39c941142
Juliano Tavares <juliano.tavaress@gmail.com>
2020-11-29 11:26:54 -0300

    Adicionada usabilidade de foco nos inputs

--- mobile/src/components/Input/index.tsx
+++ mobile/src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useImperativeHandle, forwardRef } from 'react';
+import React, { useState, useEffect, useRef, useImperativeHandle, forwardRef, useCallback } from 'react';
 import { TextInputProps } from 'react-native'
 import { useField } from '@unform/core'
 
@@ -23,6 +23,22 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({ name, ico
   const { registerField, defaultValue ='', fieldName, error } = useField(name)
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue})
 
+  const [isFocused, setIsFocused] = useState(false)
+  const [isFilled, setIsFilled] = useState(false)
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true)
+  }, [])
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false)
+
+    setIsFilled(!!inputValueRef.current.value)
+
+  }, [])
+
+
+
   useImperativeHandle(ref, () => ({
     focus() {
       inputElementRef.current.focus()
@@ -46,18 +62,20 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({ name, ico
   }, [fieldName, registerField])
 
  return (
-  <Container>
-  <Icon name={icon} size={20} color="#666360" />
-  <TextInput
-    ref={inputElementRef}
-    keyboardAppearance="dark"
-    placeholderTextColor="#666360"
-    defaultValue={defaultValue}
-    onChangeText={ value => {
-      inputValueRef.current.value = value
-    }}
-    {...rest}
-  />
+  <Container isFocused={isFocused}>
+    <Icon name={icon} size={20} color={isFocused || isFilled ? '#FF9000' : '#666360'} />
+      <TextInput
+        ref={inputElementRef}
+        keyboardAppearance="dark"
+        placeholderTextColor="#666360"
+        defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        onChangeText={ value => {
+          inputValueRef.current.value = value
+        }}
+        {...rest}
+      />
 </Container>
  )
 }
