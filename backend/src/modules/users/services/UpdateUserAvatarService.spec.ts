584a28df18b6742b877ba945e42c9a30abf72013
Juliano Tavares <juliano.tavaress@gmail.com>
2020-12-01 16:51:39 -0300

    Teste de update de avatar

---
+++ backend/src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,77 @@
+import AppError from '@shared/errors/AppError';
+import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+import FakeUsersRepository from '../repositories/fake/FakeUserRepository';
+import UpdateUserAvatarService from './UpdateUserAvatarService';
+
+describe('UpdateUserAvatar', () => {
+  it('should be able to update avatar', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeStorageProvider = new FakeStorageProvider()
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+      )
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: '123456',
+    })
+
+  await updateUserAvatar.execute({
+    user_id: user.id,
+    avatarFilename: 'avatar.jpg',
+  });
+
+  expect(user.avatar).toBe('avatar.jpg');
+
+  });
+
+  it('should not be able to update avatar from non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeStorageProvider = new FakeStorageProvider()
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+      )
+
+    expect(updateUserAvatar.execute({
+      user_id: 'non-existing-user',
+      avatarFilename: 'avatar.jpg',
+    })
+  ).rejects.toBeInstanceOf(AppError)
+
+  });
+
+  it('should delete old avatar when updating new one', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeStorageProvider = new FakeStorageProvider()
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+      )
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: '123456',
+    })
+
+  await updateUserAvatar.execute({
+    user_id: user.id,
+    avatarFilename: 'avatar.jpg',
+  });
+
+  await updateUserAvatar.execute({
+    user_id: user.id,
+    avatarFilename: 'avatar2.jpg',
+  });
+
+  expect(user.avatar).toBe('avatar.jpg');
+
+  });
+
+});
