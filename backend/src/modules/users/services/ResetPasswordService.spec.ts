fef4e12dd35e0527968939004a3464f4881ae808
Juliano Tavares <juliano.tavaress@gmail.com>
2020-12-02 17:45:20 -0300

    Test reset password with non existing user

--- backend/src/modules/users/services/ResetPasswordService.spec.ts
+++ backend/src/modules/users/services/ResetPasswordService.spec.ts
@@ -1,4 +1,5 @@
 // import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fake/FakeUserRepository';
 import FakeUserTokensRepository from '../repositories/fake/FakeUserTokensRepository';
@@ -45,9 +46,48 @@ describe('ResetPasswordService', () => {
     expect(updatedUser?.password).toBe('123123')
   })
 
-  it('', async () => {
+  it('should not be able to reset the password with non existing token', async () => {
+    await expect(
+      resetPasswordService.execute({
+        token: 'non-existing-token',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
 
   })
 
+  it('should not be able to reset the password with non existing user', async () => {
+    const { token } = await fakeUserTokensRepository.generate('non-existing-user')
+
+    await expect(
+      resetPasswordService.execute({
+        token,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+
+  })
+
+  it('should not be able to reset the password if passed more than 2 hours', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: '123456',
+    })
+
+    const { token } = await fakeUserTokensRepository.generate(user.id)
+
+    const generateHash = jest.spyOn(fakeHashProvider, 'generateHash')
+
+    await resetPasswordService.execute({
+      password: '123123',
+      token,
+    })
+
+    const updatedUser = await fakeUsersRepository.findById(user.id)
+
+    expect(generateHash).toHaveBeenCalledWith('123123')
+    expect(updatedUser?.password).toBe('123123')
+  })
 
 })
