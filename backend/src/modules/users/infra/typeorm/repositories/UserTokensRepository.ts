d14bf3211941c4f5dac2ed135611a959f70e699b
Juliano Tavares <juliano.tavaress@gmail.com>
2020-12-02 19:00:41 -0300

    Fix UserRepository

--- backend/src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
+++ backend/src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
@@ -1,6 +1,6 @@
 import { getRepository, Repository } from 'typeorm';
 
-import IUserTokensRepository from '@modules/users/repositories/IUsersRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 import UserToken from '../entities/UserToken'
@@ -23,13 +23,12 @@ class UserTokensRepository implements IUserTokensRepository {
   public async generate(user_id: string): Promise<UserToken> {
     const userToken = this.ormRepository.create({
       user_id,
-    })
+    });
 
-    await this.ormRepository.save(userToken)
+    await this.ormRepository.save(userToken);
 
-    return userToken
+    return userToken;
   }
-
 }
 
 export default UserTokensRepository;
