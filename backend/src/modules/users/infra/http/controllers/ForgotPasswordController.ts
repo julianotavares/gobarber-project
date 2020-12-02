a49aeb4c0174f41bc705ef58b1e646c3493c447c
Juliano Tavares <juliano.tavaress@gmail.com>
2020-12-02 18:33:29 -0300

    Added ForgotPasswordController

---
+++ backend/src/modules/users/infra/http/controllers/ForgotPasswordController.ts
@@ -0,0 +1,21 @@
+/* eslint-disable class-methods-use-this */
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import SendForgotPasswordEmailService from '@modules/users/services/SendForgotPasswordEmailService';
+
+export default class ForgotPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+
+    const sendForgotPasswordEmail = container.resolve(SendForgotPasswordEmailService);
+
+    await sendForgotPasswordEmail.execute({
+      email,
+    });
+
+    delete user.password;
+
+    return response.status(204).json();
+  }
+}
