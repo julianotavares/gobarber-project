1d9d2fcb1850026e495ec75ca179fdf062c9e9ad
Juliano Tavares <juliano.tavaress@gmail.com>
2020-12-05 16:51:41 -0300

    fix dashboard styles

--- mobile/src/pages/Dashboard/styles.ts
+++ mobile/src/pages/Dashboard/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
