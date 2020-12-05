057e8fe5ef98b061350c30cd06e66073c428c80a
Juliano Tavares <juliano.tavaress@gmail.com>
2020-12-05 19:08:30 -0300

    added appointmentDataPicker

--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -1,31 +1,31 @@
-import styled from 'styled-components/native'
-import { Platform } from 'react-native'
-import {getBottomSpace } from 'react-native-iphone-x-helper'
+import styled from "styled-components/native";
+import { Platform } from "react-native";
+import { getBottomSpace } from "react-native-iphone-x-helper";
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 0 30px;
-padding: 0 30px ${Platform.OS === 'android' ? 120 : 40}px;
-
-`
+  padding: 0 30px ${Platform.OS === "android" ? 150 : 40}px;
+`;
 
 export const Title = styled.Text`
   font-size: 24px;
   color: #f4ede8;
-  font-family: 'RobotoSlab-Medium'
+  font-family: "RobotoSlab-Medium";
   margin: 64px 0 24px;
-`
+`;
+
 export const ForgotPassword = styled.TouchableOpacity`
   margin-top: 24px;
-`
+`;
 
 export const ForgotPasswordText = styled.Text`
   color: #f4ede8;
   font-size: 16px;
-  font-family: 'RobotoSlab-Regular';
-`
+  font-family: "RobotoSlab-Regular";
+`;
+
 export const CreateAccountButton = styled.TouchableOpacity`
   position: absolute;
   left: 0;
@@ -35,16 +35,14 @@ export const CreateAccountButton = styled.TouchableOpacity`
   border-top-width: 1px;
   border-color: #232129;
   padding: 16px 0 ${16 + getBottomSpace()}px;
-
   justify-content: center;
   align-items: center;
   flex-direction: row;
-`
+`;
 
 export const CreateAccountButtonText = styled.Text`
-  color: #FF9000;
+  color: #ff9000;
   font-size: 18px;
-  font-family: 'RobotoSlab-Regular';
+  font-family: "RobotoSlab-Regular";
   margin-left: 16px;
-
-`
+`;
