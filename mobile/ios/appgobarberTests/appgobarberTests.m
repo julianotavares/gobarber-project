14143ddfecdaac40a38c48e4e23abe0d4516f2dd
Juliano Tavares <juliano.tavaress@gmail.com>
2020-12-17 14:53:48 -0300

    Added build

--- mobile/ios/appgobarberTests/appgobarberTests.m
+++ mobile/ios/appgobarberTests/appgobarberTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface appgobarberTests : XCTestCase
+@interface orusbarberTests : XCTestCase
 
 @end
 
-@implementation appgobarberTests
+@implementation orusbarberTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
