bb79a8acde7db4b5c0510c83b5ec02dacd87ef4b
Juliano Tavares <juliano.tavaress@gmail.com>
2020-12-18 16:52:37 -0300

    Fix rename app

--- mobile/android/app/src/main/java/com/orusbarberapp/MainActivity.java
+++ mobile/android/app/src/main/java/com/orusbarberapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.appgobarber;
+package com.orusbarberapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "appgobarber";
+    return "OrusBarberApp";
   }
 }
